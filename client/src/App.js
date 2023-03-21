f2ab3826ec53bff0a1058d6c228bcd80aa15792d
HossainMaruf <maruficepustian@gmail.com>
2023-03-21 15:04:39 +0600

    Refactoring

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import FileUpload from "./component/FileUpload";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
